07a644854e4753d9cef709bac2d6866584037faf
bShinzato <bshinzato@outlook.com>
2023-05-30 11:59:48 -0700

    Story 2 complete.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import './App.css'
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+  const gameHandleClick = (index) => {
+    // Declare new variable called updateBoard. Duplicate Array from line 5.
+    let updateBoard = [...squares]
+    // Give value to index(our parameter).
+    updateBoard[index] = "❌"
+    // Within the argument of setSquares, insert updateBoard[index]. This updates our state variable squares, our value in line 25.
+    setSquares(updateBoard)
+
+
+  }
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <div className='gameboard'>
         {squares.map((value, index) => {
-          return <Square />
+          return (
+          <Square
+          value={value}
+          index={index}
+          gameHandleClick={gameHandleClick}
+          key={index}
+          
+          />)
+          
         })}
         
       </div>
